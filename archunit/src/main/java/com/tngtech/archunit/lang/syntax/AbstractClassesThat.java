ee00d11e605ad81c59cdce5df84a5484c419bff9
AndersonSN21 <anderson.sabino.barboza.silva@ccc.ufcg.edu.br>
2026-07-17 13:35:44 -0300

    refac

--- archunit/src/main/java/com/tngtech/archunit/lang/syntax/AbstractClassesThat.java
+++ archunit/src/main/java/com/tngtech/archunit/lang/syntax/AbstractClassesThat.java
@@ -1,8 +1,21 @@
+/*
+ * Copyright 2014-2026 TNG Technology Consulting GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.tngtech.archunit.lang.syntax;
 
 import java.lang.annotation.Annotation;
-import java.util.function.Function;
-
 import com.tngtech.archunit.base.DescribedPredicate;
 import com.tngtech.archunit.core.domain.JavaAnnotation;
 import com.tngtech.archunit.core.domain.JavaClass;
@@ -14,8 +27,6 @@ import com.tngtech.archunit.core.domain.JavaMethod;
 import com.tngtech.archunit.core.domain.JavaModifier;
 import com.tngtech.archunit.core.domain.JavaStaticInitializer;
 import com.tngtech.archunit.lang.syntax.elements.ClassesThat;
-import com.tngtech.archunit.lang.syntax.elements.GivenMembersConjunction;
-
 import static com.tngtech.archunit.base.DescribedPredicate.doNot;
 import static com.tngtech.archunit.base.DescribedPredicate.not;
 import static com.tngtech.archunit.core.domain.JavaClass.Predicates.ANNOTATIONS;
